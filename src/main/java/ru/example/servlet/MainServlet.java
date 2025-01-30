08177db8f2e8e47317ae5cddd245f30c414eace4
vera <verapark.al@icloud.com>
2025-01-30 21:58:28 +0300

    Annotation Config

--- src/main/java/ru/example/servlet/MainServlet.java
+++ src/main/java/ru/example/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ru.example.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.example.controller.PostController;
 import ru.example.repository.PostRepository;
 import ru.example.service.PostService;
@@ -13,9 +15,11 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+    final var context = new AnnotationConfigApplicationContext("ru.example");
+
+    final var repository = context.getBean(PostRepository.class);
+    final var service = context.getBean(PostService.class);
+    controller = context.getBean(PostController.class);
   }
 
   @Override
